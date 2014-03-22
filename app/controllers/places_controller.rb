4d5697faf3de8cdff6294934a371102358766c10
Deven <devend711@gmail.com>
2014-03-22 01:40:57 -0400

    can't add places with duplicate names

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,16 +8,30 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new(place_params)
-    if @place.save
-      redirect_to @place
+
+    if Place.where(:name => place_params[:name]).blank?
+      @place = Place.new(place_params)
     else
-      render 'new'
+      place_exists_with_name place_params[:name]
+      render 'place_exists_with_name'
     end
+
+    unless @place.nil?
+      if @place.save
+        redirect_to @place
+      else
+        render 'new'
+      end
+    end
+
   end
 
   def index
     @places = Place.all
+  end
+
+  def place_exists_with_name(name) #render an error page if the user tried to create a place that already exists
+    @name = name
   end
 
   private
