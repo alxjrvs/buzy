ef0ed31f00c1effd107aa8f833bedc1bbfd8773c
Kanav <kdhir@bu.edu>
2014-03-22 02:07:28 -0400

    kanav's changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -21,7 +21,7 @@ class VotesController < ApplicationController
   private
 
     def vote_params
-     params.require(:vote).permit(:score, :place_id)
+     params.require(:vote).permit(:score)
     end
 
 end
