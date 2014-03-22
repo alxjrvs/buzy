b31bec93008e98872cec1f1a8890ce29f997ff93
Kanav <kdhir@bu.edu>
2014-03-22 01:56:29 -0400

    votes are working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,4 +4,24 @@ class VotesController < ApplicationController
   	@vote = Vote.new
   end
 
+  def show
+  	@vote = Vote.find(params[:id])
+  end
+
+
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to @vote
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def vote_params
+     params.require(:vote).permit(:score, :place_id)
+    end
+
 end
