dbc273103f65b45f17c25da01c3151adabe1056d
Alex Wong <alexwong@bu.edu>
2014-03-22 15:53:04 -0400

    Finish delete for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322091736) do
+ActiveRecord::Schema.define(version: 20140322190918) do
 
   create_table "places", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140322091736) do
     t.string   "password"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
